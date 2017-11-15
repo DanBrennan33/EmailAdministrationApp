210d8f34217863280e1b06289c19165581f1ae7a
DanBrennan33 <DanBrennan33@hotmail.com>
2017-11-15 15:17:18 -0500

    Inital project folder and Java files for Email Administration

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,4 +7,34 @@ public class Email {
 	private String department;
 	private int mailboxCapacity;
 	private String alternateEmail;
+	
+	// Construct an email with the follow syntax: firstname.lastname@company.com
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	// Determine the department
+			
+			
+	// Generate random String for password
+	
+	
+	/*
+	 * Have set methods:
+	 * Change the password
+	 * set the mailbox capacity
+	 * define an alternate email address
+	 * 
+	 */
+	
+	
+	/*
+	 * Have get methods to display:
+	 * name
+	 * email
+	 * mailbox capacity
+	 * 
+	 */
+	
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,32 +3,6 @@ package emailapp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// Generate an email with the follow syntax: firstname.lastname@company.com
-		
-		
-		// Determine the department
-		
-		
-		// Generate random String for password
-		
-		
-		/*
-		 * Have set methods:
-		 * Change the password
-		 * set the mailbox capacity
-		 * define an alternate email address
-		 * 
-		 */
-		
-		
-		/*
-		 * Have get methods to display:
-		 * name
-		 * email
-		 * mailbox capacity
-		 * 
-		 */
-		
 		
 	}
 
