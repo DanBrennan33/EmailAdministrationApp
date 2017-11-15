e1e9d372261c8d7e561ca972e4588261da60dfc9
DanBrennan33 <DanBrennan33@hotmail.com>
2017-11-15 15:33:43 -0500

    Department now set for User's Email information.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -12,10 +14,30 @@ public class Email {
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+		
+		// Call a method asking for department - return department
+		this.department = setDepartment();
+		
+		System.out.println("DEPARTMENT: " + this.department);
 	}
 	
 	// Determine the department
-			
+	private String setDepartment() {
+		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+		Scanner input = new Scanner(System.in);
+		int departmentNum = input.nextInt();
+		switch (departmentNum) {
+			case 1:
+				return "sales";
+			case 2:
+				return "dev";
+			case 3:
+				return "acct";
+			default:
+				return "";
+		}
+	}
 			
 	// Generate random String for password
 	
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,7 +3,7 @@ package emailapp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		
+		Email user1 = new Email("John", "Doe");
 	}
 
 }
