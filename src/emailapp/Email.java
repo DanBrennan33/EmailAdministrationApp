acd1f038c1b0fc2fb6aac8e23ca0fa59a13dd41e
DanBrennan33 <DanBrennan33@hotmail.com>
2017-11-15 15:46:51 -0500

    Generate random Password function added to User Email.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private int mailboxCapacity;
+	private int defaultPasswordLength = 10;
 	private String alternateEmail;
 	
 	// Construct an email with the follow syntax: firstname.lastname@company.com
@@ -16,10 +17,14 @@ public class Email {
 		this.lastName = lastName;
 		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 		
-		// Call a method asking for department - return department
+		// Call a method to set department - return department
 		this.department = setDepartment();
 		
 		System.out.println("DEPARTMENT: " + this.department);
+		
+		// Call a method to set random password - return Randomly generated password
+		this.password = setRandomPassword(defaultPasswordLength);
+		System.out.println("PASSWORD: " + this.password);
 	}
 	
 	// Determine the department
@@ -27,20 +32,35 @@ public class Email {
 		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
 		Scanner input = new Scanner(System.in);
 		int departmentNum = input.nextInt();
+		String dpt;
 		switch (departmentNum) {
 			case 1:
-				return "sales";
+				dpt = "sales";
+				break;
 			case 2:
-				return "dev";
+				dpt = "dev";
+				break;
 			case 3:
-				return "acct";
+				dpt = "acct";
+				break;
 			default:
-				return "";
+				dpt = "";
+				break;
 		}
+		return dpt;
 	}
 			
 	// Generate random String for password
-	
+	private String setRandomPassword(int length) {
+		String passwordAlphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+		char[] password = new char[length];
+		for (int i =0; i < length; i++) {
+			int rand = (int)(Math.random() * passwordAlphaNum.length());
+			password[i] = passwordAlphaNum.charAt(rand);
+		}
+			
+		return new String(password);
+	}
 	
 	/*
 	 * Have set methods:
